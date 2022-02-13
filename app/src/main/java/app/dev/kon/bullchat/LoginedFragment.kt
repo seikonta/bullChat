d83bf805152a142e3c310c719b983b54e5b8b0bf
seikonta <s.takasu0602@gmail.com>
2022-02-13 11:39:46 +0900

    画像のアップロード実装中

--- app/src/main/java/app/dev/kon/bullchat/LoginedFragment.kt
+++ app/src/main/java/app/dev/kon/bullchat/LoginedFragment.kt
@@ -1,14 +1,19 @@
 package app.dev.kon.bullchat
 
+import android.content.ContentResolver
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.media.MediaBrowserServiceCompat.RESULT_OK
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -18,12 +23,17 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.fragment_logined.*
+import java.lang.Exception
 import kotlin.math.log
 
 class LoginedFragment: Fragment() {
     private lateinit var auth: FirebaseAuth
     val db = Firebase.firestore
     lateinit var storage: FirebaseStorage
+
+    companion object {
+        private const val READ_REQUEST_CODE: Int = 42
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -93,10 +103,39 @@ class LoginedFragment: Fragment() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != RESULT_OK) {
+            return
+        }
+        when (requestCode) {
+            READ_REQUEST_CODE -> {
+                try {
+                    data?.data?.also { uri ->
+                    }
+                }
+                catch(e: Exception) {}
+            }
+        }
+    }
+
     private fun replaceFragment(fragemnt: Fragment) {
         val fragmentMangaer = requireActivity().supportFragmentManager
         val fragmentTransaction = fragmentMangaer.beginTransaction()
         fragmentTransaction.replace(R.id.groupFragmentContainerView, fragemnt)
         fragmentTransaction.commit()
     }
+
+    // 画像を取得して画像データを返す
+    fun selectPhoto() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+    // 画像データを元にそれをCloud Strageにアップロードする
+    fun uploadPhot() {}
 }
